5ccd6efec775098fad03feb270001cfc53055a34
Yiqi Zhang <>
2020-05-16 14:47:11 -0400

    bottom bar done

--- src/pages/Home/BottomBar.js
+++ src/pages/Home/BottomBar.js
@@ -1,16 +1,36 @@
 import React from "react";
 
-import { BottomContainerDiv, BottomIconDiv } from "./Home.styles";
+import HomeIcon from "../../assets/img/Home.png";
+import SearchIcon from "../../assets/img/Search.png";
+import LikeIcon from "../../assets/img/Like.png";
+import AddIcon from "../../assets/img/Add.png";
+
+import {
+  BottomContainerDiv,
+  BottomIconDiv,
+  BottomIconImage,
+  IconContainerDiv,
+} from "./Home.styles";
 
 const BottomBar = () => {
   return (
     <BottomContainerDiv>
       <BottomIconDiv>
-        <p>Home</p>
-        <p>Search</p>
-        <p>Post</p>
-        <p>Like</p>
-        <p>Prof</p>
+        <IconContainerDiv>
+          <BottomIconImage src={HomeIcon} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={SearchIcon} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={AddIcon} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={LikeIcon} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={HomeIcon} alt="" />
+        </IconContainerDiv>
       </BottomIconDiv>
     </BottomContainerDiv>
   );
