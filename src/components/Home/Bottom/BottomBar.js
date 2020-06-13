5d86545275b2d7749109783abe0a03206f0f77d9
Yiqi Zhang <>
2020-06-13 07:23:23 -0400

    skeleton setup

--- src/components/Home/Bottom/BottomBar.js
+++ src/components/Home/Bottom/BottomBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 //Helpers
 import { bottomIcons } from "../../../Helpers/Icons";
 
@@ -12,14 +14,29 @@ import {
 } from "./BottomBar.styles.js";
 
 const BottomBar = () => {
+  const linkStyles = { display: "flex", height: "3rem", alignItems: "center" };
   return (
     <BottomContainerDiv>
       <BottomIconDiv>
-        {bottomIcons.map((icon) => (
-          <IconContainerDiv>
-            <BottomIconImage src={icon} alt="" />
-          </IconContainerDiv>
-        ))}
+        <IconContainerDiv>
+          <Link to="/" style={linkStyles}>
+            <BottomIconImage src={bottomIcons[0]} alt="" />
+          </Link>
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={bottomIcons[1]} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={bottomIcons[2]} alt="" />
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <Link to="/activity" style={linkStyles}>
+            <BottomIconImage src={bottomIcons[3]} alt="" />
+          </Link>
+        </IconContainerDiv>
+        <IconContainerDiv>
+          <BottomIconImage src={bottomIcons[4]} alt="" />
+        </IconContainerDiv>
       </BottomIconDiv>
     </BottomContainerDiv>
   );
---
+++ src/pages/Activity/Activity.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Components
+import BottomBar from "../../components/Home/Bottom/BottomBar";
+
+// Styles
+import { OutterContainerDiv } from "../Home/Home.styles";
+
+const Activity = () => {
+  return (
+    <>
+      <OutterContainerDiv>
+        <BottomBar />
+      </OutterContainerDiv>
+    </>
+  );
+};
+
+export default Activity;
